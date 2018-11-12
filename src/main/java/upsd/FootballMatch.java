1c6d53fb64a0ab0985445b89fac63a778405020d
upsd <up648421@myport.ac.uk>
2018-11-12 16:52:51 +0000

    Remove score fields, replacing them with map that links enum to score.

--- src/main/java/upsd/FootballMatch.java
+++ src/main/java/upsd/FootballMatch.java
@@ -1,41 +1,42 @@
 package upsd;
 
+import java.util.HashMap;
+
 public class FootballMatch {
 
     private final int scoreLimit;
-    private int homeScore;
-    private int awayScore;
+    private HashMap<Team, Integer> scores = new HashMap<>();
 
     public FootballMatch(int scoreLimit) {
         this.scoreLimit = scoreLimit;
+        scores.put(Team.HOME, 0);
+        scores.put(Team.AWAY, 0);
     }
 
     public FootballMatch(int scoreLimit, int homeScore, int awayScore) {
         this.scoreLimit = scoreLimit;
-        this.homeScore = homeScore;
-        this.awayScore = awayScore;
+        scores.put(Team.HOME, homeScore);
+        scores.put(Team.AWAY, awayScore);
     }
 
     public String score() {
-        return homeScore + "-" + awayScore;
+        return this.scores.get(Team.HOME) + "-" + this.scores.get(Team.AWAY);
     }
 
     public void scoreFor(Team team) {
         if (!status().contains("won")) {
-            if (team.equals(Team.HOME)) {
-                homeScore += 1;
-            } else {
-                awayScore += 1;
-            }
+            int score = this.scores.get(team);
+
+            this.scores.put(team, score + 1);
         }
     }
 
     public String status() {
-        if (homeScore == scoreLimit) {
+        if (this.scores.get(Team.HOME) == scoreLimit) {
             return "Home team has won";
         }
 
-        if (awayScore == scoreLimit) {
+        if (this.scores.get(Team.AWAY) == scoreLimit) {
             return "Away team has won";
         }
 
